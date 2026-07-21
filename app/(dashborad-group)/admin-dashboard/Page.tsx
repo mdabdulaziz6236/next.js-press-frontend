dddd98e79f44430bd2f9f750575859f87976efe2
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-07-21 06:22:08 +0600

    Redirecting Users To Role Based Routes After Successful Login

--- app/(dashborad-group)/admin-dashboard/Page.tsx
+++ app/(dashborad-group)/admin-dashboard/Page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const AdminDashboardPage = () => {
-  return (
-    <div>AdminDashboardPage</div>
-  )
-}
+const AdminDashboard = () => {
+  return <div>AdminDashboard</div>;
+};
 
-export default AdminDashboardPage
+export default AdminDashboard;
